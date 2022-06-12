5ef35dc5a96d9c82d2e19bc27552785d5f36c75a
Hp <Hp@192.168.5.91>
2022-06-11 21:57:11 -0400

    This is my first Group deliverable1

---
+++ src/practicing/the/fit/PracticingTheFit.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicing.the.fit;
+
+/**
+ *
+ * @author Hp
+ */
+public class PracticingTheFit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+     
+        
+        System.out.println("hello everyone");
+    }
+    
+}
